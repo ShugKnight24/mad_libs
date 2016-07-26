1c7aa090dff8afaf790e22a35d41a5b408ad9677
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-26 00:22:19 -0400

    Seperate controllers into two script files.

--- js/formcontroller.js
+++ js/formcontroller.js
@@ -1,4 +1,4 @@
-// use strict;
+"use strict";
 
 var madLibs = angular.module("madLibs");
 
@@ -21,22 +21,4 @@ madLibs.controller("madLibsFormController",["$scope", function($scope, madLibSer
     madLibService.lastName2 = $scope.lastName2;
     $location.path("/view");
   }
-}]);
-
-madLibs.controller("madLibsViewController",["$scope", function($scope, madLibService){
-  $scope.lastname = madLibService.lastname ;
-  $scope.verb = madLibService.verb;
-  $scope.loveInterest = madLibService.loveInterest;
-  $scope.bodyPart = madLibService.bodyPart;
-  $scope.femaleName = madLibService.femaleName;
-  $scope.verb2 = madLibService.verb2;
-  $scope.noun = madLibService.noun;
-  $scope.plural = madLibService.plural;
-  $scope.verb3 = madLibService.verb3;
-  $scope.noun2 = madLibService.noun2;
-  $scope.occupation = madLibService.occupation;
-  $scope.salary = madLibService.salary;
-  $scope.verb4 = madLibService.verb4;
-  $scope.firstName = madLibService.firstName;
-  $scope.lastName2 = madLibService.lastName2;
 }]);
